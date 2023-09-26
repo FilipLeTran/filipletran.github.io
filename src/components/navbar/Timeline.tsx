917a32cfd8fd99abc0fbf8d73d2d189fabe26abf
Filip Tran <filipletran@gmail.com>
2023-09-26 17:36:56 +0200

    add timeline path

--- src/components/navbar/Timeline.tsx
+++ src/components/navbar/Timeline.tsx
@@ -1,10 +1,23 @@
-import React, { Component } from 'react'
-import Airplane from './Airplane'
+import React, { Component } from 'react';
+import Airplane from './Airplane';
+import { styled } from '@mui/system';
+
+const Path = styled('div')({
+    position: 'absolute',
+    top: 0,
+    left: '5vw',
+    backgroundColor: 'white',
+    height: '100vh',
+    width: 2
+});
 
 export default class Timeline extends Component {
   render() {
     return (
-        <Airplane></Airplane>
+        <div>
+            <Airplane></Airplane>
+            <Path></Path>
+        </div>
     )
   }
 }
