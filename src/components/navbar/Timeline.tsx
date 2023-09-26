47d2b5c7f4a72e0a949b4cd87a6056659aec774c
Filip Tran <filipletran@gmail.com>
2023-09-26 16:36:59 +0200

    add timeline

---
+++ src/components/navbar/Timeline.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Airplane from './Airplane'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+        <Airplane></Airplane>
+    )
+  }
+}
