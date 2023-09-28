11e9e85351c9ae50f35540b17c22502758e1d589
Filip Tran <filipletran@gmail.com>
2023-09-28 16:01:03 +0200

    add tailwind

--- src/components/content/About-Me/ProgrammingLanguages.tsx
+++ src/components/content/About-Me/ProgrammingLanguages.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function ProgrammingLanguages() {
   return (
-    <div>ProgrammingLanguages</div>
+    <div>ProgrammingLanguages
+        
+    </div>
   )
 }
